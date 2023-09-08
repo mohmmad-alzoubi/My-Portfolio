096ac5869ea15c62c85b95d6dbb7ac99b60465e1
Mohmmad Nabeel <mohmmadcs585@gmail.com>
2023-09-08 16:58:07 +0300

    new

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings: ['Frontend Developer' , 'YouTuber' , 'Web Developer'],
+    strings: ['welcome to my portfolio i hope at amazing..' , 'Frontend Developer..' , 'Backend Developer..' , 'Full Stack..'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
